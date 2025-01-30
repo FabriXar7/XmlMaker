5f704819b24a9850b6cbd6b967d5e63331935064
Fabrixar <fabrixar@icloud.com>
2025-01-30 10:18:13 -0300

    Update LMix.py

--- LMix.py
+++ LMix.py
@@ -1,9 +1,7 @@
 import os
-
 import imageio
 import xml.etree.ElementTree as ET
 from moviepy import VideoFileClip
-
 
 ruta_programas = 'O:\\Fabrix\\LMix\\PROGRAMAS\\'
 ruta_publicidad = 'O:\\Fabrix\\LMix\\PUBLICIDAD\\'
@@ -15,6 +13,7 @@ extensiones_multimedia = ['.mp4', '.avi', '.mkv', '.mp3', '.wav', '.flac']
 archivos_programas = []
 archivos_publicidad = []
 
+
 def segundosA(segundos):
     horas = int(segundos / 60 / 60)
     segundos -= horas*60*60
@@ -22,43 +21,25 @@ def segundosA(segundos):
     segundos -= minutos*60
     return f"{horas:02d}:{minutos:02d}:{segundos:02d}"
 
-for archivo in os.listdir(ruta_programas):
 
-    ruta_completa = os.path.join(ruta_programas, archivo)
-    
-
-    if os.path.isfile(ruta_completa) and any(archivo.lower().endswith(ext) for ext in extensiones_multimedia):
-        try:
-
-            if archivo.lower().endswith(('.mp4', '.avi', '.mkv')):
-                clip = VideoFileClip(ruta_completa)
-            
-
-            duracion = segundosA(int(clip.duration))
-            archivos_programas.append((archivo, duracion))
-            clip.close()
-        except Exception as e:
-            print(f"Error al procesar el archivo {archivo}: {e}")
+def listarProgramas(ruta):
+    archivos = []
+    for archivo in os.listdir(ruta):
+        ruta_completa = os.path.join(ruta, archivo)
+        if os.path.isfile(ruta_completa) and any(archivo.lower().endswith(ext) for ext in extensiones_multimedia):
+            try:
+                if archivo.lower().endswith(('.mp4', '.avi', '.mkv')):
+                    clip = VideoFileClip(ruta_completa)
+                duracion = segundosA(int(clip.duration))
+                archivos.append((archivo, duracion))
+                clip.close()
+            except Exception as e:
+                print(f"Error al procesar el archivo {archivo}: {e}")    
+    return archivos
 
 
-for archivo in os.listdir(ruta_publicidad):
-
-    ruta_completa = os.path.join(ruta_publicidad, archivo)
-    
-
-    if os.path.isfile(ruta_completa) and any(archivo.lower().endswith(ext) for ext in extensiones_multimedia):
-        try:
-
-            if archivo.lower().endswith(('.mp4', '.avi', '.mkv')):
-                clip = VideoFileClip(ruta_completa)
-            
-
-            duracion = segundosA(int(clip.duration))
-            archivos_publicidad.append((archivo, duracion))
-            clip.close()
-        except Exception as e:
-            print(f"Error al procesar el archivo {archivo}: {e}")
-
+archivos_programas = listarProgramas(ruta_programas)
+archivos_publicidad = listarProgramas(ruta_publicidad)
 
 
 vMixManager = ET.Element("vMixManager")
@@ -100,4 +81,5 @@ for nombre, duracion in archivos_programas:
 tree = ET.ElementTree(vMixManager)
 tree.write("PlayList.xml",encoding='utf-8',xml_declaration=True)
 print("XML creado correctamente.")
+
 
